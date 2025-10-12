9625a4d4fecbef9df7e3694c422b68a407ae72d3
rsm100991 <rsm100991@gmail.com>
2025-10-12 18:52:52 +0530

    final push

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+    base: "/smiles-project/", // <- Add this line
   plugins: [
     react(),
     runtimeErrorOverlay(),
